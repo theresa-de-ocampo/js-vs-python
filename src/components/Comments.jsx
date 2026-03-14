855cd800a321aba38da820cbb3624c87624af479
theresa-de-ocampo <ma_theresa7@yahoo.com>
2026-03-14 21:02:25 +0800

    Change Subheader

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,31 +1,32 @@
-import CheatSheet from './generic/CheatSheet.jsx'
+import CheatSheet from "./generic/CheatSheet.jsx";
 
 export default function Comments() {
   const rows = [
     {
-      description: 'Single Line Comment',
-      javascript: [['// This is a comment']],
-      python: [['# This is a comment']],
+      description: "Single Line Comment",
+      javascript: [["// This is a comment"]],
+      python: [["# This is a comment"]]
     },
     {
-      description: 'Multi Line Comment',
-      javascript: [['/*', 'This is a multiline', 'comment', '*/']],
+      description: "Multi Line Comment",
+      javascript: [["/*", "This is a multiline", "comment", "*/"]],
       python: [
         [
           "'''",
-          'Python does not have a dedicated syntax',
-          'for multiline comments.',
-          'The use of triple quotes is discouraged',
-          'as it is actually a string literal.',
-          'If it appears on top of a module, class, or function,',
-          'Python treats it as a docstring.',
-          'So using triple quotes can accidentally',
-          'modify documentation.',
-          "'''",
-        ],
-      ],
-    },
-  ]
+          "No dedicated syntax",
+          "for multiline comments.",
+          "Use of triple quotes is discouraged",
+          "as it is actually a string literal.",
+          "If it appears on top of a module,",
+          "class, or function,",
+          "Python treats it as a docstring.",
+          "Using triple quotes can accidentally",
+          "modify documentation.",
+          "'''"
+        ]
+      ]
+    }
+  ];
 
-  return <CheatSheet title="Comments" rows={rows} />
+  return <CheatSheet title="Comments" rows={rows} />;
 }

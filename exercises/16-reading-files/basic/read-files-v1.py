70e5b70db88419caf88d6273b65619c58f10e2ee
theresa-de-ocampo <ma_theresa7@yahoo.com>
2026-04-01 23:24:18 +0800

    fix: Old File Reader

--- exercises/16-reading-files/basic/read-files-v1.py
+++ exercises/16-reading-files/basic/read-files-v1.py
@@ -1,33 +1,30 @@
-# from pathlib import Path
+from pathlib import Path
 
-# file_path = Path(__file__).with_name("files/hello.txt")
+file_path = Path(__file__).resolve().parent.parent / "files" / "hello.txt"
 
-# with file_path.open("r") as file:
-#     print(file.read())
-
-a = open("files/hello.txt", "r")
+a = open(file_path, "r")
 print("***** Read All *****")
 print(a.read())
 a.close()
 
 print("\n***** Read First 10 Chars *****")
-b = open("files/hello.txt", "r")
+b = open(file_path, "r")
 print(b.read(10))
 b.close()
 
 
 print("\n***** Read First 2 Lines *****")
-c = open("files/hello.txt", "r")
+c = open(file_path, "r")
 print(c.readline(), end="")
 print(c.readline())
 c.close()
 
 print("\n***** readlines - keeps \\n *****")
-d = open("files/hello.txt", "r")
+d = open(file_path, "r")
 print(d.readlines())
 d.close()
 
 print("\n***** splitlines - removes \\n *****")
-e = open("files/hello.txt", "r")
+e = open(file_path, "r")
 print(e.read().splitlines())
 e.close()
